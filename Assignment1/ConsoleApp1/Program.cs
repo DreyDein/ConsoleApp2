9ff664ec8a228ad674c6c6aa62967c52cd303029
DreyDein <158158202+DreyDein@users.noreply.github.com>
2024-02-12 06:50:15 -0800

    Add files via upload

---
+++ Assignment1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonWeightTracker
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            Console.WriteLine("Welcome to the Person Weight Tracker!");
+
+            // Asking user to input people's data
+            while (true)
+            {
+                Console.WriteLine("Enter the person's name (or type 'done' to finish):");
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "done")
+                    break;
+
+                Console.WriteLine("Enter the person's weight:");
+                int weight;
+                if (!int.TryParse(Console.ReadLine(), out weight))
+                {
+                    Console.WriteLine("Invalid weight. Please enter a valid integer.");
+                    continue;
+                }
+
+                // Creating a new person object
+                Person person = new Person
+                {
+                    Name = name,
+                    Weight = weight
+                };
+
+                // Adding the person to the list
+                people.Add(person);
+            }
+
+            // Displaying all names and calculating total weight
+            int totalWeight = 0;
+            Console.WriteLine("\nPeople and their weights:");
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"{person.Name}: {person.Weight}kg");
+                totalWeight += person.Weight;
+            }
+
+            Console.WriteLine($"\nTotal weight of all people: {totalWeight}kg");
+
+            Console.WriteLine("\nThank you for using the Person Weight Tracker!");
+        }
+    }
+}
+
+
